3cf3fe8d494b3cbe9ee97119f4e998ab542bb9c2
arfannabiell <162048018+arfannabiell@users.noreply.github.com>
2025-04-19 08:41:19 +0700

    Refactor: Replace long parameter list in calculateTax() with TaxData object

--- TaxCalculator/src/lib/Employee.java
+++ TaxCalculator/src/lib/Employee.java
@@ -99,6 +99,16 @@ public class Employee {
 			monthWorkingInYear = 12;
 		}
 		
-		return TaxFunction.calculateTax(monthlySalary, otherMonthlyIncome, monthWorkingInYear, annualDeductible, spouseIdNumber.equals(""), childIdNumbers.size());
+		TaxData taxData = new TaxData(
+    		monthlySalary,
+    		otherMonthlyIncome,
+    		monthWorkingInYear,
+    		annualDeductible,
+    		!spouseIdNumber.equals(""),
+    		childIdNumbers.size()
+		);
+
+		return TaxFunction.calculateTax(taxData);
+
 	}
 }
